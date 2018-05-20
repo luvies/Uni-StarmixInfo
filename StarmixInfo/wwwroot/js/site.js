8f11795df42772c6adcf47acd9a9fee987e76c9a
Luke Davies <lkdvs3@gmail.com>
2018-05-20 19:40:09 +0100

    Added game load button to reduce homepage lag

--- StarmixInfo/wwwroot/js/site.js
+++ StarmixInfo/wwwroot/js/site.js
@@ -1,1 +1,14 @@
-﻿// Write your JavaScript code.
+﻿'use strict';
+
+// game loading
+var gameInstance = null;
+var loadGame = () => {
+    gameInstance = UnityLoader.instantiate("gameContainer", "/game/Build/webGL.json", {
+        onProgress: UnityProgress
+    });
+};
+
+// game window hiding
+if ($(window).width() < 768) {
+    $('#central-hall').addClass('hide-game');
+}
